fcafa3cce28ff0f3b92e1ff3472ee41910853e03
Zunuoh <audponu@gmail.com>
2022-02-09 09:28:30 +0000

    pages created

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import HomeScreen from './pages/homeScreen';
+import SignIn from './auth/signin';
+import SignUp from './auth/signup';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {GlobalProvider} from '../src/context/GlobalState';
+
+function App() {
+  return (
+ 
+  );
+
+}
+
+export default App;
---
+++ src/screens/SideScreen.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SideScreen = () => {
+  return (
+    <div>SideScreen</div>
+  )
+}
+
+export default SideScreen
